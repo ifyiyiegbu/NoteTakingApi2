26a25858e8444461f4a94b6cd02a8111700473cf
ifyiyiegbu <ifeyinwaiyiegbu@gmail.com>
2025-03-16 18:16:36 +0100

    fixing category issues

--- src/models/noteModel.ts
+++ src/models/noteModel.ts
@@ -12,7 +12,7 @@ const NoteSchema: Schema = new Schema(
   {
     title: { type: String, required: true, trim: true, maxlength: 200 },
     content: { type: String, required: true, trim: true, maxlength: 5000 },
-    category: { type: Schema.Types.ObjectId, ref: "Category", required: true, index: true },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: false, index: true },
   },
   { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
 );
